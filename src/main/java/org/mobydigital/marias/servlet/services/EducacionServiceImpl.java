0b3db96c83fb963b560062d0cff3e849afceab00
Matias Arias <matiasarias384@gmail.com>
2023-04-11 11:34:27 -0300

    Create Educacion.class, Service and implementation

---
+++ src/main/java/org/mobydigital/marias/servlet/services/EducacionServiceImpl.java
@@ -0,0 +1,15 @@
+package org.mobydigital.marias.servlet.services;
+
+import org.mobydigital.marias.servlet.models.Educacion;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class EducacionServiceImpl implements EducacionService{
+
+    @Override
+    public List<Educacion> listar() {
+        return Arrays.asList(new Educacion(1L,"Instituo Gral. Manuel Belgrano","Bachiller en Informatica",2013,2018),
+                new Educacion(2L,"Universidad Tecnologica Nacional FRVM","Ingenieria en Sistemas de información",2019,2024));
+    }
+}
