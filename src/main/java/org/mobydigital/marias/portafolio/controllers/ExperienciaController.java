35ec97634ccf3184199875dcd87f3a11e5329fc4
Matias Arias <matiasarias384@gmail.com>
2023-04-14 11:01:42 -0300

    Add GET,POST,PUT,DELETE ExperienciaController

--- src/main/java/org/mobydigital/marias/portafolio/controllers/ExperienciaController.java
+++ src/main/java/org/mobydigital/marias/portafolio/controllers/ExperienciaController.java
@@ -1,6 +1,7 @@
 package org.mobydigital.marias.portafolio.controllers;
 
 import jakarta.ws.rs.QueryParam;
+import org.mobydigital.marias.portafolio.models.Educacion;
 import org.mobydigital.marias.portafolio.models.Experiencia;
 import org.mobydigital.marias.portafolio.services.ExperienciaService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,8 +21,25 @@ public class ExperienciaController {
     public ResponseEntity<List<Experiencia>> getExperiencias(@QueryParam("experiencia") String experiencia){
         return new ResponseEntity<List<Experiencia>>(experienciaService.getListEntidades(experiencia), HttpStatus.OK);
     }
+
+    @GetMapping(value="/{id}")
+    public ResponseEntity<Experiencia> getExperienciaPorId(@PathVariable("id") Long id){
+        return new ResponseEntity<Experiencia>(experienciaService.porId(id),HttpStatus.OK);
+    }
+
     @PostMapping
     public ResponseEntity<Experiencia> guardarExperiencia(@RequestBody Experiencia experiencia){
         return new ResponseEntity<Experiencia>(experienciaService.guardar(experiencia),HttpStatus.CREATED);
     }
+
+    @PutMapping
+    public ResponseEntity<Experiencia> actualizarExperiencia(@RequestBody Experiencia experiencia){
+        return new ResponseEntity<Experiencia>(experienciaService.guardar(experiencia),HttpStatus.OK);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable("id") Long id){
+        experienciaService.eliminar(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
 }
