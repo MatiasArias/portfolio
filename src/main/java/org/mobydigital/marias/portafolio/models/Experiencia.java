75fbcffa97bc1dee336e8520524ac36d4be1f542
Matias Arias <matiasarias384@gmail.com>
2023-04-14 09:55:07 -0300

    Create Experiencia.class

---
+++ src/main/java/org/mobydigital/marias/portafolio/models/Experiencia.java
@@ -0,0 +1,74 @@
+package org.mobydigital.marias.portafolio.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="experiencia")
+public class Experiencia {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idExperiencia;
+    private String titulo;
+    private String descripcion;
+    private Integer añoDesde;
+    private Integer añoHasta;
+
+    public Experiencia(Long idExperiencia, String titulo, String descripcion, Integer añoDesde, Integer añoHasta) {
+        this.idExperiencia = idExperiencia;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.añoDesde = añoDesde;
+        this.añoHasta = añoHasta;
+    }
+
+    public Experiencia() {
+    }
+
+    public Long getIdExperiencia() {
+        return idExperiencia;
+    }
+
+    public void setIdExperiencia(Long idExperiencia) {
+        this.idExperiencia = idExperiencia;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Integer getAñoDesde() {
+        return añoDesde;
+    }
+
+    public void setAñoDesde(Integer añoDesde) {
+        this.añoDesde = añoDesde;
+    }
+
+    public Integer getAñoHasta() {
+        return añoHasta;
+    }
+
+    public void setAñoHasta(Integer añoHasta) {
+        this.añoHasta = añoHasta;
+    }
+
+    @Override
+    public String toString() {
+        return  idExperiencia +" - " + titulo  +
+                "\n" + añoDesde +
+                "-" + añoHasta +
+                "\n\t" + descripcion + '\'' ;
+    }
+}
