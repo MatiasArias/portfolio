12e49959a6bff78cb83868d3701f1454c857b348
Matias Arias <matiasarias384@gmail.com>
2023-04-16 14:17:20 -0300

    Refactor Get Path and Request

--- src/main/java/org/mobydigital/marias/portafolio/configuration/Pages.java
+++ src/main/java/org/mobydigital/marias/portafolio/configuration/Pages.java
@@ -3,4 +3,7 @@ package org.mobydigital.marias.portafolio.configuration;
 public class Pages {
     public static String HOME="index";
     public static String PROYECTO="proyecto";
+    public static String LOGIN="login";
+    public static String BUSCAR="buscar";
+
 }
--- src/main/java/org/mobydigital/marias/portafolio/controllers/ProyectoController.java
+++ src/main/java/org/mobydigital/marias/portafolio/controllers/ProyectoController.java
@@ -8,28 +8,25 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/proyecto")
 public class ProyectoController {
     @Autowired
     private ProyectoService proyectoService;
 
-@GetMapping("/")
-    public ModelAndView getProyectos(){
-    ModelAndView modelAndView = new ModelAndView(Pages.HOME);
-    modelAndView.addObject("proyectos", proyectoService.getListEntidades(""));
+@GetMapping("/p/{proyecto}")
+    public ModelAndView getProyectoIndividual(
+             @PathVariable(required=true,name="proyecto") Long id){
+    ModelAndView modelAndView = new ModelAndView(Pages.PROYECTO);
+    modelAndView.addObject("proyecto",proyectoService.porId(id));
     return modelAndView;
 }
-@GetMapping("/proyecto")
-    public ModelAndView getProyectoIndividual(
-            @RequestParam(defaultValue = "1",name = "id",required = false) Long id
-    ){
+@GetMapping("")
+    public ModelAndView getProyectoIndividualRequest(
+            @RequestParam(defaultValue="1",name = "id",required = false) Long id){
     ModelAndView modelAndView = new ModelAndView(Pages.PROYECTO);
     modelAndView.addObject("proyecto",proyectoService.porId(id));
     return modelAndView;
