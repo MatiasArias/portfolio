b63cd80240508b2a8da85020befc1f85e2660a0c
Matias Arias <matiasarias384@gmail.com>
2023-04-20 16:28:06 -0300

    Add Candidato
    Add Service, Controller, Repository

---
+++ src/main/java/org/mobydigital/marias/portafolio/repositories/CandidatoRepository.java
@@ -0,0 +1,45 @@
+package org.mobydigital.marias.portafolio.repositories;
+
+import jakarta.persistence.EntityManager;
+import org.mobydigital.marias.portafolio.models.entities.Candidato;
+import org.mobydigital.marias.portafolio.models.entities.Educacion;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+
+import java.util.List;
+@Repository
+public class CandidatoRepository implements CrudRepository<Candidato>{
+
+    @Autowired
+    private EntityManager em;
+
+    public CandidatoRepository() {
+    }
+
+    @Override
+    public List<Candidato> listar() {
+        return em.createQuery("SELECT c FROM Candidato c", Candidato.class).getResultList();
+    }
+
+    @Override
+    public Candidato porId(Long id) {
+        return em.find(Candidato.class,id);
+    }
+
+    @Override
+    public void guardar(Candidato candidato) {
+        if (candidato.getIdCandidato() !=null && candidato.getIdCandidato() > 0){
+            em.merge(candidato);
+        }
+        else{
+            em.persist(candidato);
+        }
+    }
+
+    @Override
+    public void eliminar(Long id) {
+        Candidato candidato = porId(id);
+        em.remove(candidato);
+    }
+}
