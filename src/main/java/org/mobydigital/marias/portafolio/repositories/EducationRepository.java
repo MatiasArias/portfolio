6618b8d5bbb309cd8944fc550fbbf53f74079999
Matias Arias <matiasarias384@gmail.com>
2023-05-02 11:59:09 -0300

    Refactor repositories

--- src/main/java/org/mobydigital/marias/portafolio/repositories/EducationRepository.java
+++ src/main/java/org/mobydigital/marias/portafolio/repositories/EducationRepository.java
@@ -1,11 +1,9 @@
 package org.mobydigital.marias.portafolio.repositories;
 
-import org.mobydigital.marias.portafolio.models.entities.Habilidad;
+import org.mobydigital.marias.portafolio.models.entities.Education;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 @Repository
-public interface HabilidadRepository extends JpaRepository<Habilidad, Long> {
-
+public interface EducationRepository extends JpaRepository<Education, Long> {
 }
